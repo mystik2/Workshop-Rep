f6844e6dded915bc11b2230cc200815135efc501
Michael Stekly <m.stekly@gmx.de>
2012-03-15 11:11:19 +0100

    Registrierungs/Einloggbox hochgeladen

---
+++ Registrierung_Einloggen_Miki/Registrierung_Einloggen_Miki/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Registrierung_Einloggen_Miki;
+
+namespace Registrierung_Einloggen_Miki
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btbRegistrieren_Click(object sender, RoutedEventArgs e)
+        {
+            User Benutzer1 = new User();
+
+            Benutzer1.maxZeichenlaengeUsername(txtRegistrierUsername.ToString());
+            Benutzer1.minZeichenlaengePassword(txtRegistrierPassword.ToString());
+
+            MessageBox.Show("hier Username");
+            MessageBox.Show("heir PW");
+        }
+
+       
+    }
+}
---
+++ Registrierung_Einloggen_Miki/Registrierung_Einloggen_Miki/User.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+
+
+namespace Registrierung_Einloggen_Miki
+{
+    class User
+    {
+        String sUsername;
+        String sPassword;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="eingabePassword"></param>
+        public void minZeichenlaengePassword(String eingabePassword)
+        {
+            int iLenght;
+            iLenght = eingabePassword.Length;
+            if (iLenght < 6)
+            {
+                MessageBox.Show("Ihre Eingabe muss mindestens 6 Zeichen besitzen!");
+            }
+            else
+            {
+                this.setPassword(eingabePassword);
+            }
+
+        }
+
+        public void maxZeichenlaengeUsername(String eingabeUsername)
+        {
+            int iLenght;
+            iLenght = eingabeUsername.Length;
+            if (iLenght > 15)
+            {
+                MessageBox.Show("Ihre Eingabe darf maximal 15 Zeichen besitzen!");
+            }
+            else
+            {
+                this.setUsername(eingabeUsername);
+            }
+
+        }
+       private void setUsername(string p)
+        {
+            this.sUsername = p;
+        }
+
+        private void setPassword(string p)
+        {
+            this.sPassword = p;
+        }
+
+        private void getUsername(string p)
+        {
+            this.sPassword = p;
+        }
+
+        private void getPassword(string p)
+        {
+            this.sPassword = p;
+        }
+    }
+
+}
